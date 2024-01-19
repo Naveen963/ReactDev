66eeb007231682f83f8d87701b1675091d71b75d
Naveen <naveenkumar200101@gmail.com>
2024-01-19 17:05:46 +0530

    Search functionality added

--- Cuisine-FoodOrderApp/src/components/MainPageLayout.jsx
+++ Cuisine-FoodOrderApp/src/components/MainPageLayout.jsx
@@ -5,11 +5,15 @@ import Shimmer from "./Shimmer";
 
 export const MainPageLayout = () => {
     const [restaurantList, setRestaurantList] = useState([]);
-
+    const [filteredList, setFilteredList] = useState([]);
     const ratingFilterHandler = () => {
-        setRestaurantList(restaurantList.filter((c) => c.info.avgRating > 4.3))
+        setFilteredList(restaurantList.filter((c) => c.info.avgRating > 4.3))
     }
 
+    const searchFilterHandler = (searchtext) => {
+        console.log(searchtext)
+        setFilteredList(restaurantList.filter((c) => c.info.name.toLowerCase().includes(searchtext.toLowerCase())))
+    }
     useEffect(() => {
         fetchRestaurantData()
     }, [])
@@ -18,16 +22,16 @@ export const MainPageLayout = () => {
         const res = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.9407367&lng=82.2291264&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const { data } = await res.json();
 
-        setRestaurantList(data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setRestaurantList(data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredList(data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
     return <div className="body">
-        <Search />
+        <Search searchFilter={searchFilterHandler} />
         <div className="filter">
             <button className="filter-btn" onClick={ratingFilterHandler}>📈 Top Rated Restaurants</button>
         </div>
         <div className="res-container">
-
-            {restaurantList.length == 0 ? Array.from({ length: 20 }).map(c => <Shimmer />) : restaurantList.map((restaurant, idx) => (
+            {filteredList.length == 0 ? Array.from({ length: 20 }).map(c => <Shimmer />) : filteredList.map((restaurant, idx) => (
                 <RestaurantCard
                     key={restaurant.info.id}
                     restaurant={restaurant.info}
--- Cuisine-FoodOrderApp/src/components/Search.jsx
+++ Cuisine-FoodOrderApp/src/components/Search.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
 
 
-const Search = () => {
+const Search = (props) => {
+    const [search, setSearch] = useState("")
     return (
-        <div className="search">Search</div>
+        <div className="search">
+            <input className="searchBox" type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search...."
+                onKeyUp={(e) => e.key == 'Enter' && props.searchFilter(search)} />
+            <button className="search-btn" onClick={() => props.searchFilter(search)} >Search</button>
+        </div>
     )
 }
 
